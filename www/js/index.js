d16ffdb28768b71a98501f49ca6bc1f3b532de24
PHIB16 <phib1_16@uni.worc.ac.uk>
2017-11-06 14:04:01 +0000

    added stuff

---
+++ www/js/index.js
@@ -0,0 +1,21 @@
+
+
+
+window.localStorage.setItem("age", "19");
+window.localStorage.setItem("email", "cuck@cuckmail.com");
+window.localStorage.setItem("Name",  "Ronnie McDonnie");
+
+var age = window.localStorage.getItem("age");
+var email = window.localStorage.getItem("email");
+var Name = window.localStorage.getItem("Name");
+
+
+	
+$( document ).ready(function() {
+    console.log( "ready!" );
+    
+    $("#age").text(age);
+	$("#email").text(email);
+	$("#Name").text(Name);
+
+});
