de9c742b852ccdf16506b8d57c0bc4677158d8e9
PHIB16 <phib1_16@uni.worc.ac.uk>
2017-11-06 14:29:08 +0000

    text up

--- www/js/index.js
+++ www/js/index.js
@@ -1,18 +1,15 @@
-<!-- For more info on jQuery Mobile,  touch gestures and other useful events see : http://api.jquerymobile.com/category/events/ -->
-
 $(document).on("pagecreate","#pageone",function(){
-  
-  	$('#tapholdtext').on("taphold",function(){
-    	$(this).hide();
- 	});                       
-
-	$('#taptext').on("tap",function(){
-    	alert("Stop tapping!");
- 	}); 
-
-	$('#swipetext').on("swipeleft",function(){
-    	$(this).css('color', 'green');
-  	});   
+  $('#submitButton').on("click", function(){
+    submitText();
+  });            
+});            
 
 
-});
+function submitText() {
+	var text = $('#textinput').val();
+	alert(text);
+}
+
+function storeValue(key, value) {
+	//add some code to store the key-value pair in persistant storage 
+}
